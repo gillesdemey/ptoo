48a39288569c0f843ec02a6a427e9200ae3762fb
Gilles De Mey <gilles.de.mey@gmail.com>
2012-12-16 02:28:26 +0100

    Debuging like a boss

--- app/assets/javascripts/posts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-	$("[data-toggle=collapse]").click(function(){
-		$(this).next().find(".collapse").collapse('toggle');
-		$(this).find("i").toggleClass('icon-chevron-up').toggleClass('icon-chevron-down');
-	});
-
-});
--- app/models/category.rb
+++
@@ -1,9 +0,0 @@
-class Category < ActiveRecord::Base
-  attr_accessible :name
-
-  belongs_to :post
-
-  def to_s
-  	read_attribute :name
-  end
-end
